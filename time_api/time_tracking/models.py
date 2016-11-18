a4e0656d4158c66b0bfeb407ac42cd95ece1630d
jypweb <jypemail@gmail.com>
2016-11-18 14:59:36 -0700

    Profile mumbo jumbo.

--- time_api/time_tracking/models.py
+++ time_api/time_tracking/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Project(models.Model):
@@ -23,6 +25,34 @@ class Task(models.Model):
     def __str__(self):
         return self.name
 
+class Profile(models.Model):
+    PROFILE_TYPES = (
+        ('MN', 'Manager'),
+        ('WB', 'WaterBoy'),
+        ('UI', 'Unpaid Intern'),
+        ('SD', 'Senior Developer'),
+        ('NO', 'Nobody')
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_type = models.CharField(
+        max_length=2,
+        choices=PROFILE_TYPES,
+        default='NO',
+    )
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return str(self.user)
 
 class Entry(models.Model):
     user = models.ForeignKey(User, related_name='time_entries')
